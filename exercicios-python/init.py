608294bfab4bfa3593550ed08fe26d4566327f9d
GabrielCStabile <devstabile@gmail.com>
2025-06-07 13:00:04 -0300

    Adicionando primeiro exercício da JDD

--- exercicios-python/init.py
+++
@@ -1,1 +0,0 @@
-python ("Iniciando o repositório de exercícios.")
---
+++ exercicios-python/jdd_a1_ex1.py
@@ -0,0 +1,4 @@
+# Crie um programa que o usuário digite o nome e retorne o nº de caracteres.
+
+nome = input ("Digite o seu nome: ")
+print(f"O nome do {nome} tem {len(nome)} letras.")
