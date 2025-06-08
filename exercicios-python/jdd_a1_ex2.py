f6770698fcac9f1062cda656a1e30ebd451b37bf
GabrielCStabile <devstabile@gmail.com>
2025-06-08 20:09:58 -0300

    Resolvendo o segundo exercício da aula 1.

---
+++ exercicios-python/jdd_a1_ex2.py
@@ -0,0 +1,6 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+valor_1 = int(input("Digite o primeiro valor: "))
+valor_2 = int(input("Digite o segundo valor: "))
+
+print(f"O valor da somatória é: {valor_1 + valor_2}")
